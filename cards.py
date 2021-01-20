0c8010909af6576ed6351aef22fbf3c66aca8096
Dawid Kowalczyk <dawid.kowalczyk1@hotmail.com>
2021-01-20 21:48:02 +0100

    Changed GameData to Deck. Now it contains lists of instances of new classes Player and Pile instead of 10 lists, easier to maintain code and reduces boilerplate code. Modified function that loads data from api to a new game, added a function to play a card

--- cards.py
+++ cards.py
@@ -38,21 +38,35 @@ class Card:
     code: str
 
 
-@dataclass()
-class GameData:
-    deck_id: str = field(init=False)
-    left_pile_visible: Card = field(init=False)
-    right_pile_visible: Card = field(init=False)
-    player_one_name: str
-    player_two_name: str
-    player_one_hand: List[Card] = field(default_factory=list)
-    player_one_visible: List[Card] = field(default_factory=list)
-    player_two_hand: List[Card] = field(default_factory=list)
-    player_two_visible: List[Card] = field(default_factory=list)
-    left_pile: List[Card] = field(default_factory=list)
-    right_pile: List[Card] = field(default_factory=list)
+@dataclass
+class Player:
+    name: str
+    hand: List[Card] = field(default_factory=list)
+    hidden: List[Card] = field(default_factory=list)
 
-    def load_data(self):
+
+@dataclass
+class Pile:
+    visible: Card = field(init=False)
+    hidden: List[Card] = field(default_factory=list)
+
+
+@dataclass()
+class Deck:
+    PREVIOUS_CARD = {'2': 'ACE', '3': '2', '4': '3', '5': '4', '6': '5', '7': '6', '8': '7', '9': '8', '0': '9',
+                     'JACK': '0',
+                     'QUEEN': 'JACK', 'KING': 'QUEEN', 'ACE': 'KING'}
+    NEXT_CARD = {'2': '3', '3': '4', '4': '5', '5': '6', '6': '7', '7': '8', '8': '9', '9': '0', '0': 'JACK',
+                 'JACK': 'QUEEN', 'QUEEN': 'KING', 'KING': 'ACE', 'ACE': '2'}
+    deck_id: str = field(init=False)
+    players: List[Player] = field(default_factory=list)
+    piles: List[Pile] = field(default_factory=list)
+
+    def load_data_new_game(self, name_one: str, name_two: str):
+        self.players.append(Player(name=name_one))
+        self.players.append(Player(name=name_two))
+        self.piles.append(Pile())
+        self.piles.append(Pile())
         new_deck = requests.get("https://deckofcardsapi.com/api/deck/new/shuffle")
         deck_id = new_deck.json()['deck_id']
         response = requests.get('https://deckofcardsapi.com/api/deck/' + deck_id + '/draw/?count=52')
@@ -60,32 +74,45 @@ class GameData:
         codes = list()
         for el in response.get('cards'):
             codes.append(el)
-            print(el)
-        print(len(codes))
         for i in range(0, 15):
-            self.player_one_hand \
-                .append(
-                Card(image=codes[i]['image'], value=codes[i]['value'], suit=codes[i]['suit'], code=codes[i]['code']))
-            self.player_two_hand \
-                .append(Card(image=codes[i + 15]['image'], value=codes[i + 15]['value'], suit=codes[i + 15]['suit'],
-                             code=codes[i + 15]['code']))
+            for j in range(0, 2):
+                self.players[j].hidden \
+                    .append(Card(image=codes[i + (j * 15)]['image'], value=codes[i + (j * 15)]['value'],
+                                 suit=codes[i + (j * 15)]['suit'],
+                                 code=codes[i + (j * 15)]['code']))
         for i in range(30, 35):
-            self.player_one_visible \
-                .append(
-                Card(image=codes[i]['image'], value=codes[i]['value'], suit=codes[i]['suit'], code=codes[i]['code']))
-            self.player_two_visible \
-                .append(Card(image=codes[i + 5]['image'], value=codes[i + 5]['value'], suit=codes[i + 5]['suit'],
-                             code=codes[i + 5]['code']))
+            for j in range(0, 2):
+                self.players[j].hand \
+                    .append(
+                    Card(image=codes[i + j * 5]['image'], value=codes[i + j * 5]['value'],
+                         suit=codes[i + j * 5]['suit'],
+                         code=codes[i + j * 5]['code']))
         for i in range(36, 40):
-            self.left_pile \
-                .append(
-                Card(image=codes[i]['image'], value=codes[i]['value'], suit=codes[i]['suit'], code=codes[i]['code']))
-            self.right_pile \
-                .append(Card(image=codes[i + 5]['image'], value=codes[i + 5]['value'], suit=codes[i + 5]['suit'],
-                             code=codes[i + 5]['code']))
-        self.left_pile_visible = Card(image=codes[50]['image'], value=codes[50]['value'], suit=codes[50]['suit'],
-                                      code=codes[50]['code'])
-        self.right_pile_visible = Card(image=codes[51]['image'], value=codes[51]['value'], suit=codes[51]['suit'],
-                                       code=codes[51]['code'])
+            for j in range(0, 2):
+                self.piles[j].hidden \
+                    .append(
+                    Card(image=codes[i + j * 5]['image'], value=codes[i + j * 5]['value'],
+                         suit=codes[i + j * 5]['suit'],
+                         code=codes[i + j * 5]['code']))
+        self.piles[0].visible = Card(image=codes[50]['image'], value=codes[50]['value'], suit=codes[50]['suit'],
+                                     code=codes[50]['code'])
+        self.piles[1].visible = Card(image=codes[51]['image'], value=codes[51]['value'], suit=codes[51]['suit'],
+                                     code=codes[51]['code'])
 
+    def play_card(self, player_id: int, pile_id: int, card_id: int):
+        if self.can_be_placed(player_id, pile_id, card_id):
+            self.piles[pile_id].hidden.append(self.piles[pile_id].visible)
+            self.piles[pile_id].visible = self.players[player_id].hand[card_id]
+            self.players[player_id].hand[card_id] = None
+            for i in range(0, 5):
+                print(self.players[player_id].hand[i])
+
+
+    def can_be_placed(self, player_id: int, pile_id, card_id):
+        print(self.players[player_id].hand[card_id].value)
+        print(self.piles[pile_id].visible.value)
+        if (self.PREVIOUS_CARD.get(str(self.players[player_id].hand[card_id].value)) == self.piles[pile_id].visible.value or
+                self.NEXT_CARD.get(str(self.players[player_id].hand[card_id].value)) == self.piles[pile_id].visible.value):
+            return True
+        return False
 
